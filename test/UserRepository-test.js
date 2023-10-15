f13c35a946245324a0af4aef4e9c8bf667bee2d7
Bobby Steckline <rjsturing@gmail.com>
2023-10-15 14:24:39 -0400

    Add function imports to UserRepository-test

--- test/UserRepository-test.js
+++ test/UserRepository-test.js
@@ -1,7 +1,14 @@
-import { expect } from 'chai';
+import { expect } from "chai";
+import {
+  getUserData,
+  calculateAvgStepGoal,
+  getRandomUser
+  //functions to be tested, all of them
+} from "src/scripts.js";
+import userData from "src/data/users.js";
 
-describe('User Repository', () => {
-  it('should run tests', function () {
+describe("User Repository", () => {
+  it("should run tests", function () {
     expect(true).to.be(true);
   });
 });
