ded020260c2207819f6ddfa709c5dd7e99e18759
Deanna Stevens <deanna.sofias@gmail.com>
2023-10-17 22:14:46 -0400

    Add getWeeklyHydration

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,7 @@
 
 
-// Hydration functions
+// HYDRATION FUNCTIONS
+
 const getAvgTotalFluid = (data, id) => {
   if(!data || !id) {
     return undefined
@@ -12,12 +13,31 @@ const getAvgTotalFluid = (data, id) => {
   }, 0)
   return Math.round(hydrationAvg / hydrationEntries.length)
 }
+// console.log(getAvgTotalFluid(hydrationData, 1))
 
-console.log(getAvgTotalFluid(hydrationData, 1))
+const getDayFluids = (data, id, date) => {
+  if (!data || !id || !date) {
+    return undefined
+  }
+  const hydrationEntries = data.filter((entry) => entry.userID === id)
+  const dailyEntry = hydrationEntries.find((entry) => entry.date === date)
+  return dailyEntry.numOunces
+}
+// console.log(getDayFluids(hydrationData, 1, "2023/03/24" )
 
-
+const getWeeklyHydration = (hydrationData, userId) => {
+  const userHydrationData = hydrationData.filter(data => data.userID === userId);
+  const weeklyOunces = userHydrationData.map(data => ({
+    date: data.date,
+    ounces: data.numOunces
+  }));
+  return weeklyOunces;
+};
+// console.log(getWeeklyHydration(hydrationData, 31))
 
 
 export {
-
+  getAvgTotalFluid,
+  getDayFluids,
+  getWeeklyHydration
 }
