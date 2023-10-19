38092f118b96f46a866e62d0f4110d75bd445559
Deanna Stevens <deanna.sofias@gmail.com>
2023-10-19 19:44:31 -0400

    Add sleep fetch

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -30,3 +30,19 @@ fetch("https://fitlit-api.herokuapp.com/api/v1/users")
 .catch((error) => {
     console.error("error", error);
 });
+
+
+
+fetch("https://fitlit-api.herokuapp.com/api/v1/sleep")
+.then((response) => {
+    if(!response.ok) {
+        throw new Error("Error");
+    }
+    return response.json();
+})
+.then((data) => {
+    console.log(data);
+})
+.catch((error) => {
+    console.error("error", error);
+});
