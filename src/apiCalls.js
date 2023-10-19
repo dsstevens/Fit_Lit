dbdd75d0a5b437d0e057afd57f4d6f1ad157d5d4
Deanna Stevens <deanna.sofias@gmail.com>
2023-10-19 19:41:34 -0400

    Add activity fetch

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,4 +1,18 @@
 // Your fetch requests will live here!
+fetch("https://fitlit-api.herokuapp.com/api/v1/activity")
+.then((response) => {
+    if(!response.ok) {
+        throw new Error("Error");
+    }
+    return response.json();
+})
+.then((data) => {
+    console.log(data);
+})
+.catch((error) => {
+    console.error("error", error);
+});
+
 
 
 console.log('I will be a fetch request!')
