86534b5ebc3445eb7e4df4e3f59ece75616b93ce
Gavin Garcia <edwardgavingarcia@yahoo.com>
2023-10-18 19:33:56 -0500

    Fix bug for displaying dom information/update html

--- src/scripts.js
+++ src/scripts.js
@@ -1,10 +1,13 @@
-// This is the JavaScript entry file - your code begins here
+// This is the JavaScript entry file - your code begins her
 const getUserData = (users, userId) => {
-  return users.find(user => user.id === userId);
+  return users.find((user) => user.id === userId);
 };
 
-const calculateAvgStepGoal = users => {
-  const totalStepGoal = users.reduce((sum, { dailyStepGoal }) => sum + dailyStepGoal, 0); 
+const calculateAvgStepGoal = (users) => {
+  const totalStepGoal = users.reduce(
+    (sum, { dailyStepGoal }) => sum + dailyStepGoal,
+    0
+  );
   return totalStepGoal / users.length;
 };
 
@@ -12,30 +15,25 @@ const getRandomUser = (users) => {
   const randomIndex = Math.floor(Math.random() * users.length);
   const randomUser = users[randomIndex];
   return randomUser;
-}
-
+};
 
 // Do not delete or rename this file ********
 
-
 // An example of how you tell webpack to use a CSS file
-import './css/styles.css';
+import "./css/styles.css";
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png';
+import "./images/turing-logo.png";
 
 // An example of how you tell webpack to use a JS file
-import userData from './data/users';
-console.log("User Data:", userData);
+import userData from "./data/users";
+// console.log("hello");
+// console.log("User Data:", userData);
 
 // Example of one way to import functions from the domUpdates file.  You will delete these examples.
-import { exampleFunction1, exampleFunction2 } from './domUpdates';
+import { loadCards } from "./domUpdates";
 
-exampleFunction1('Travis');
-exampleFunction2('Travis')
+loadCards(userData);
 
-export {
-  getUserData,
-  calculateAvgStepGoal,
-  getRandomUser
-}
+
+export { getUserData, calculateAvgStepGoal, getRandomUser };
