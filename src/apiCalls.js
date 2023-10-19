e0c25d9d4c4e762636371255f09c123aff61b1b5
Deanna Stevens <deanna.sofias@gmail.com>
2023-10-19 19:43:10 -0400

    Add users fetch

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -17,3 +17,16 @@ fetch("https://fitlit-api.herokuapp.com/api/v1/activity")
 
 console.log('I will be a fetch request!')
 
+fetch("https://fitlit-api.herokuapp.com/api/v1/users")
+.then((response) => {
+    if(!response.ok) {
+        throw new Error("Error");
+    }
+    return response.json();
+})
+.then((data) => {
+    console.log(data);
+})
+.catch((error) => {
+    console.error("error", error);
+});
