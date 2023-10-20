9da82736c596dae405decfa7d186f619158f40f3
Deanna Stevens <deanna.sofias@gmail.com>
2023-10-19 21:13:55 -0400

    Refactor fetchAPIcall function

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,63 +1,19 @@
 // Your fetch requests will live here!
-fetch("https://fitlit-api.herokuapp.com/api/v1/activity")
-.then((response) => {
+const fetchAPIcall = (data) => {
+  fetch(`"https://fitlit-api.herokuapp.com/api/v1/${data}"`)
+  .then((response) => {
     if(!response.ok) {
-        throw new Error("Error");
+      throw new Error("Error");
     }
     return response.json();
-})
-.then((data) => {
+  })
+  .then((data) => {
     console.log(data);
-})
-.catch((error) => {
+  })
+  .catch((error) => {
     console.error("error", error);
-});
-
-
+  });
+}
 
 console.log('I will be a fetch request!')
 
-fetch("https://fitlit-api.herokuapp.com/api/v1/users")
-.then((response) => {
-    if(!response.ok) {
-        throw new Error("Error");
-    }
-    return response.json();
-})
-.then((data) => {
-    console.log(data);
-})
-.catch((error) => {
-    console.error("error", error);
-});
-
-
-
-fetch("https://fitlit-api.herokuapp.com/api/v1/sleep")
-.then((response) => {
-    if(!response.ok) {
-        throw new Error("Error");
-    }
-    return response.json();
-})
-.then((data) => {
-    console.log(data);
-})
-.catch((error) => {
-    console.error("error", error);
-});
-
-
-fetch("https://fitlit-api.herokuapp.com/api/v1/hydration")
-.then((response) => {
-    if(!response.ok) {
-        throw new Error("Error");
-    }
-    return response.json();
-})
-.then((data) => {
-    console.log(data);
-})
-.catch((error) => {
-    console.error("error", error);
-});
