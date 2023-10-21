193bfedd6c3edc5bdf809ca4663d1a0fec283290
Deanna Stevens <deanna.sofias@gmail.com>
2023-10-20 21:36:02 -0400

    Add updateDom function to wrap invoked functions for the eventListener

--- src/scripts.js
+++ src/scripts.js
@@ -1,73 +1,60 @@
-// IMPORTS
 import { fetchAPIcall } from './apiCalls'
 import { updateInfoCard, updateWelcomeMessage, compareStepGoals } from './domUpdates'
-// after making sure that the user data is being imported from the api, delete the users import and reorient the users arg for several fns
+// after making sure that the user data is being imported from the api, delete the users import 
 
-
-let activityData = []
-let userData = []
-let sleepData = []
-let hydrationData = []
-// EVENTLISTENERS
+const updateDom = (allData) => {
+  const activityData = allData[0].activityData
+  const usersData = allData[1].users
+  const sleepData = allData[2].sleepData
+  const hydrationData = allData[3].hydrationData
+  const randomUser = getRandomUser(usersData);
+  const randomUserData = getUserData(usersData, randomUser.id);
+  const avgStepGoal = calculateAvgStepGoal(usersData);
+  updateWelcomeMessage(randomUser.name);
+  updateInfoCard(randomUser);
+  compareStepGoals(randomUser, avgStepGoal);
+  console.log(randomUser, randomUserData, avgStepGoal)
+  console.log(allData)
+}
+// // EVENTLISTENERS
 window.addEventListener("load", function () {
-  console.log("anystring")
-  
   Promise.all([
     fetchAPIcall('activity'),
     fetchAPIcall('users'),
     fetchAPIcall('sleep'),
     fetchAPIcall('hydration')
   ])
-  .then((response) => {
-    activityData = response[0]
-    userData = response[1]
-    sleepData = response[2]
-    hydrationData = response[3]
-    console.log("response", response)
-      
-    getRandomUser(userData);
-    getUserData(userData, randomUser.id);
-    calculateAvgStepGoal(userData);
-    updateWelcomeMessage(randomUser.name);
-    updateInfoCard(randomUser);
-    compareStepGoals(randomUser, averageStepGoal);
-
-})
+    .then((allData) => {
+      updateDom(allData)
+      // console.log(allData)
+    })
 });
-
-const getUserData = (userData, userId) => {
-  return userData.find((user) => user.id === userId);
+const getUserData = (users, userId) => {
+  return users.find((user) => user.id === userId);
 };
-
-const calculateAvgStepGoal = (userData) => {
-  const totalStepGoal = userData.reduce(
+const calculateAvgStepGoal = (users) => {
+  const totalStepGoal = users.reduce(
     (sum, { dailyStepGoal }) => sum + dailyStepGoal,
     0
   );
-  return totalStepGoal / userData.length;
+  return totalStepGoal / users.length;
 };
-
-const getRandomUser = (userData) => {
-  const randomIndex = Math.floor(Math.random() * userData.length);
-  const randomUser = userData[randomIndex];
+const getRandomUser = (users) => {
+  const randomIndex = Math.floor(Math.random() * users.length);
+  const randomUser = users[randomIndex];
   return randomUser;
 };
 
 
-// Do not delete or rename this file ********
 // An example of how you tell webpack to use a CSS file
 import "./css/styles.css";
-
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import "./images/turing-logo.png";
-
 // An example of how you tell webpack to use a JS file
 // import userData from "./data/users";
 // console.log("hello");
 // console.log("User Data:", userData);
-
 // Example of one way to import functions from the domUpdates file.  You will delete these examples.
-
 // loadCards(userData);
 
 
