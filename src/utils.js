edbf9fb764808576779cd0bc842d4c7a26d54ff6
Gavin Garcia <edwardgavingarcia@yahoo.com>
2023-10-21 14:38:49 -0600

    Creates getAvgDailySleep

--- src/utils.js
+++ src/utils.js
@@ -3,44 +3,53 @@
 // HYDRATION FUNCTIONS
 
 const getAvgTotalFluid = (data, id) => {
-  if(!data || !id) {
-    return undefined
+  if (!data || !id) {
+    return undefined;
   }
-  const hydrationEntries = data.filter((entry) => entry.userID === id)
-  console.log(hydrationEntries.length)
+  const hydrationEntries = data.filter((entry) => entry.userID === id);
+  console.log(hydrationEntries.length);
   const hydrationAvg = hydrationEntries.reduce((acc, user) => {
-    return (acc += user.numOunces)
-  }, 0)
-  return Math.round(hydrationAvg / hydrationEntries.length)
-}
+    return (acc += user.numOunces);
+  }, 0);
+  return Math.round(hydrationAvg / hydrationEntries.length);
+};
 // console.log(getAvgTotalFluid(hydrationData, 1))
 
 const getDayFluids = (data, id, date) => {
   if (!data || !id || !date) {
-    return undefined
+    return undefined;
   }
-  const hydrationEntries = data.filter((entry) => entry.userID === id)
-  const dailyEntry = hydrationEntries.find((entry) => entry.date === date)
-  return dailyEntry.numOunces
-}
+  const hydrationEntries = data.filter((entry) => entry.userID === id);
+  const dailyEntry = hydrationEntries.find((entry) => entry.date === date);
+  return dailyEntry.numOunces;
+};
 // console.log(getDayFluids(hydrationData, 1, "2023/03/24" )
 
 const getWeeklyHydration = (hydrationData, userId) => {
   if (!hydrationData || !userId) {
-    return undefined
+    return undefined;
   }
-  const userHydrationData = hydrationData.filter(data => data.userID === userId);
-  const weeklyOunces = userHydrationData.map(data => ({
+  const userHydrationData = hydrationData.filter(
+    (data) => data.userID === userId
+  );
+  const weeklyOunces = userHydrationData.map((data) => ({
     date: data.date,
-    ounces: data.numOunces
+    ounces: data.numOunces,
   }));
   return weeklyOunces;
 };
 // console.log(getWeeklyHydration(hydrationData, 31))
 
+// SLEEP FUNCTIONS:
+const getAvgDailySleep = (sleepData, userId) => {
+  if (!sleepData || !userId) {
+    return undefined;
+  }
+  const userSleepData = sleepData.filter((data) => data.userID === userId);
+  const sleepAvg = userSleepData.reduce((acc, user) => {
+    return (acc += user.hoursSlept);
+  }, 0);
+  return Math.round(sleepAvg / userSleepData.length);
+};
 
-export {
-  getAvgTotalFluid,
-  getDayFluids,
-  getWeeklyHydration
-}
+export { getAvgTotalFluid, getDayFluids, getWeeklyHydration, getAvgDailySleep };
