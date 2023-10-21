282e185da45ee5c88a62a7a9127401fd39ddf5f5
William Lavery <lavery_william@yahoo.com>
2023-10-21 11:30:29 -0400

    Add data to hydration test data file

---
+++ src/data/hydrationTestData.js
@@ -0,0 +1,40 @@
+hydrationData = {
+  userWater: [
+    { "userID": 1,
+      "date": "2023/03/24",
+      "numOunces": 28
+    },
+    { "userID": 2,
+      "date": "2023/03/24",
+      "numOunces": 35
+    },
+    { "userID": 3,
+      "date": "2023/03/24",
+      "numOunces": 95
+    }, 
+    { "userID": 1,
+      "date": "2023/03/25",
+      "numOunces": 50
+    },
+    { "userID": 2,
+      "date": "2023/03/25",
+      "numOunces": 92
+    },
+    { "userID": 3,
+      "date": "2023/03/25",
+      "numOunces": 59
+    },
+    { "userID": 1,
+      "date": "2023/03/26",
+      "numOunces": 21
+    },
+    { "userID": 2,
+      "date": "2023/03/26",
+      "numOunces": 88
+    },
+    { "userID": 3,
+      "date": "2023/03/26",
+      "numOunces": 63
+    }
+  ],
+}
