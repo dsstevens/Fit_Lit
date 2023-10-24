0c99d9be4de4887e850180a65d2687b4a9117d11
Deanna Stevens <deanna.sofias@gmail.com>
2023-10-24 17:31:43 -0400

    Delete beforeEach hooks

--- test/UserRepository-test.js
+++ test/UserRepository-test.js
@@ -332,77 +332,13 @@ describe("user's sleep", function () {
 // Return if a user reached their step goal for a given day
 
 describe("user's activity", function () {
-  let activityData;
-  beforeEach(function () {
-    userData = {
-      users: [
-        {
-          id: 1,
-          name: "Trystan Gorczany",
-          address: "9484 Lucas Flat, West Kittymouth WA 67504",
-          email: "Taurean_Pollich31@gmail.com",
-          strideLength: 4,
-          dailyStepGoal: 7000,
-          friends: [5, 43, 46, 11]
-        },
-        {
-          id: 2,
-          name: "Tyreek VonRueden",
-          address: "623 Koelpin Skyway, Lake Luigichester MN 77576-1678",
-          email: "Nicolette_Halvorson43@yahoo.com",
-          strideLength: 4.5,
-          dailyStepGoal: 8000,
-          friends: [13, 19, 3]
-        },
-        {
-          id: 3,
-          name: "Colt Rohan",
-          address: "48010 Balistreri Harbor, Cleobury IN 43317",
-          email: "Wilford.Barton@gmail.com",
-          strideLength: 2.7,
-          dailyStepGoal: 3000,
-          friends: [31, 16, 15, 7]
-        },
-      ],
-    };
-
-    activityData = {
-      userActivity: [
-        {userID: 1, date: "2023/03/24", numSteps: 7362, minutesActive: 261, flightsOfStairs: 26},
-        {userID: 2, date: "2023/03/24", numSteps: 3049, minutesActive: 125, flightsOfStairs: 43},
-        {userID: 3, date: "2023/03/24", numSteps: 12970, minutesActive: 282, flightsOfStairs: 38},
-        {userID: 1, date: "2023/03/25", numSteps: 14264, minutesActive: 111, flightsOfStairs: 1},
-        {userID: 2, date: "2023/03/25", numSteps: 14719, minutesActive: 201, flightsOfStairs: 39},
-        {userID: 3, date: "2023/03/25", numSteps: 12255, minutesActive: 245, flightsOfStairs: 46},
-        {userID: 1, date: "2023/03/26", numSteps: 8646, minutesActive: 32, flightsOfStairs: 31},
-        {userID: 2, date: "2023/03/26", numSteps: 9543, minutesActive: 203, flightsOfStairs: 34},
-        {userID: 3, date: "2023/03/26", numSteps: 10676, minutesActive: 153, flightsOfStairs: 28},
-        {userID: 1, date: "2023/03/27", numSteps: 5405, minutesActive: 54, flightsOfStairs: 42},
-        {userID: 2, date: "2023/03/27", numSteps: 12127, minutesActive: 120, flightsOfStairs: 3},
-        {userID: 3, date: "2023/03/27", numSteps: 2383, minutesActive: 285, flightsOfStairs: 38},
-        {userID: 1, date: "2023/03/28", numSteps: 8638, minutesActive: 123, flightsOfStairs: 26},
-        {userID: 2, date: "2023/03/28", numSteps: 5494, minutesActive: 89, flightsOfStairs: 46},
-        {userID: 3, date: "2023/03/28", numSteps: 6327, minutesActive: 297, flightsOfStairs: 27},
-        {userID: 1, date: "2023/03/29", numSteps: 9608, minutesActive: 53, flightsOfStairs: 22},
-        {userID: 2, date: "2023/03/29", numSteps: 6959, minutesActive: 269, flightsOfStairs: 16},
-        {userID: 3, date: "2023/03/29", numSteps: 2378, minutesActive: 219, flightsOfStairs: 11},
-        {userID: 1, date: "2023/03/30", numSteps: 14960, minutesActive: 52, flightsOfStairs: 4},
-        {userID: 2, date: "2023/03/30", numSteps: 4676, minutesActive: 288, flightsOfStairs: 5},
-        {userID: 3, date: "2023/03/30", numSteps: 2940, minutesActive: 59, flightsOfStairs: 32},
-        {userID: 1, date: "2023/03/31", numSteps: 2321, minutesActive: 111, flightsOfStairs: 33},
-        {userID: 2, date: "2023/03/31", numSteps: 14985, minutesActive: 207, flightsOfStairs: 44},
-        {userID: 3, date: "2023/03/31", numSteps: 2837, minutesActive: 85, flightsOfStairs: 29}
-      ],
-    };
-  });
-
     it('should calculate the miles walked for a specific user on a specific day', () => {
       const userID = 1;
       const date = '2023/03/24';
       const milesWalked = calculateMilesWalked(userID, date, userData.users, activityData.userActivity);
       expect(milesWalked).to.be.closeTo(5.577); 
     });
-
+ 
     it('should return 0 for a user with no activity data for the given day', () => {
       const userID = 2;
       const date = '2023/03/23'; 
