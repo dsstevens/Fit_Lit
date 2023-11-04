9613df5bfd2aa59b1daff7c49958e83cf057303a
William Lavery <lavery_william@yahoo.com>
2023-11-04 17:10:07 -0400

    Add hydration data form

--- src/scripts.js
+++ src/scripts.js
@@ -4,6 +4,15 @@ import "./images/banner.png";
 import { fetchAPIcall } from "./apiCalls";
 import { updateDom } from "./domUpdates";
 
+const hydrationFormSubmitButton = document.querySelector("#hydrationFormSubmitButton");
+const hydrationDate = document.querySelector("#hydrationDate");
+const hydrationOunces = document.querySelector("#hydrationOunces");
+
+const postHydrationData = (event) => {
+  event.preventDefault()
+  console.log(hydrationDate.value, hydrationOunces.value)
+}
+
 // // EVENT LISTENERS
 window.addEventListener("load", function () {
   Promise.all([
@@ -14,4 +23,8 @@ window.addEventListener("load", function () {
   ]).then((allData) => {
     updateDom(allData);
   });
+});
+
+hydrationFormSubmitButton.addEventListener("click", function(event) {
+  postHydrationData(event)
 });
